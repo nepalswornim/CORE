fdf9976ac34947643ed0c8ba44e2e3f12a34e22a
swornim.nepal@braindigit.com <Swornim@12345>
2017-10-30 14:57:23 +0545

    link added

--- src/CoreFirstApplication/Controller/ValuesController.cs
+++ src/CoreFirstApplication/Controller/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,38 +11,50 @@ using Microsoft.AspNetCore.Mvc;
 namespace CoreFirstApplication.Controller
 {
     [Route("api/[controller]")]
-   public  class ValuesController 
+    public class ValuesController
     {
         // GET: api/values
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
 
         // GET api/values/5
         [HttpGet("{id:int}")]
-        public string Get(int id)
+        public string Get([FromQuery]int  id, string query)
         {
-            return $"value { id}";
+            return $"value {id} query={ query}";
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Value value)
         {
+            if (!ModelState.IsValid)
+            {
+
+            }
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+        public class Value
+        {
+
+            public int Id { get; set; }
+            [MinLength(3)]
+            public string Text { get; set; }
         }
     }
 }
